58b32fc12c08cdebf4dd580c053c6540f318940a
ishmyles <ish.myles@gmail.com>
2022-05-09 17:30:45 +1000

    Added basic campground schema to models

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const port = 3000;
+const dbUrl = 'mongodb://127.0.0.1:27017/yelpCamp';
 
-const app = express();
+mongoose.connect(dbUrl)
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to Database')
+})
+
+const app = express(db);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
