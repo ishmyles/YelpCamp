a48d1ec52a1cc2f2405ac5eb7e4a47a128f99a98
ishmyles <ish.myles@gmail.com>
2022-05-09 16:59:16 +1000

    Added views & models folder

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const port = 3000;
 
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', async function (req, res) {
-    res.send('Welcome to YelpCamp!')
+    res.render('index')
 })
 
 app.listen(port, () => {
