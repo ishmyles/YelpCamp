a01ebb358807d80c5b7cdead4ad271e5f00fa41b
ishmyles <ish.myles@gmail.com>
2022-05-20 16:15:57 +1000

    Added map popups to map marker

--- public/static/js/mapbox.js
+++ public/static/js/mapbox.js
@@ -6,6 +6,10 @@ const map = new mapboxgl.Map({
     zoom: 9 // starting zoom
 });
 
+const popup = new mapboxgl.Popup()
+    .setHTML(`<h5>${camp}</h5><p>${campLocation}</p>`);
+
 const marker = new mapboxgl.Marker()
     .setLngLat(geoData)
+    .setPopup(popup)
     .addTo(map);
